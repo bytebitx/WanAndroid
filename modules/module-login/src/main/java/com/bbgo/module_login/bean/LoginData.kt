e83c528a8e6b47175f578884ec07cd81c012301c
wangyuebin <yuebin.wang-a1140@aqara.com>
2021-10-26 11:10:56 +0800

    为bean增加keep，使其不被混淆

--- modules/module-login/src/main/java/com/bbgo/module_login/bean/LoginData.kt
+++ modules/module-login/src/main/java/com/bbgo/module_login/bean/LoginData.kt
@@ -1,10 +1,13 @@
 package com.bbgo.module_login.bean
 
+import androidx.annotation.Keep
+
 /**
  *  author: wangyb
  *  date: 2021/5/21 10:26 上午
  *  description: todo
  */
+@Keep
 data class LoginData(
     val chapterTops: MutableList<String>,
     val collectIds: MutableList<String>,
