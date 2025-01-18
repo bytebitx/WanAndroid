33a9e3ccb44a968691f42a9fca502c17cd6a5ca9
wangyuebin <lon.wang@idepstech.com>
2025-01-18 16:52:24 +0800

    更新xlog配置

--- common/base/src/main/java/com/bytebitx/base/util/log/Logs.kt
+++ common/base/src/main/java/com/bytebitx/base/util/log/Logs.kt
@@ -1,7 +1,9 @@
 package com.bytebitx.base.util.log
 
+import com.bytebitx.base.BuildConfig
 import com.bytebitx.base.util.FileUtils
 import com.elvishew.xlog.LogConfiguration
+import com.elvishew.xlog.LogLevel
 import com.elvishew.xlog.XLog
 import com.elvishew.xlog.printer.AndroidPrinter
 import com.elvishew.xlog.printer.file.FilePrinter
@@ -18,6 +20,7 @@ import com.geely.common.log.LogFileNameGenerator
 object Logs {
 
     private const val LOG_TAG = "Wan"
+
     // 保存最近7天的日志
     private const val MAX_TIME = 7 * 24 * 60 * 60 * 1000L
 
@@ -26,33 +29,23 @@ object Logs {
     }
 
     private fun initConfig() {
-        val config: LogConfiguration = LogConfiguration.Builder()
-//            .logLevel(
-//                if (BuildConfig.DEBUG) LogLevel.ALL // 指定日志级别，低于该级别的日志将不会被打印，默认为 LogLevel.ALL
-//                else LogLevel.INFO
-//            )
-            .tag(LOG_TAG) // 指定 TAG，默认为 "X-LOG"
-//            .enableThreadInfo() // 允许打印线程信息，默认禁止
-//            .enableStackTrace(2) // 允许打印深度为 2 的调用栈信息，默认禁止
-            .enableBorder() // 允许打印日志边框，默认禁止
-//            .jsonFormatter(MyJsonFormatter()) // 指定 JSON 格式化器，默认为 DefaultJsonFormatter
-//            .xmlFormatter(MyXmlFormatter()) // 指定 XML 格式化器，默认为 DefaultXmlFormatter
-//            .throwableFormatter(MyThrowableFormatter()) // 指定可抛出异常格式化器，默认为 DefaultThrowableFormatter
-//            .threadFormatter(MyThreadFormatter()) // 指定线程信息格式化器，默认为 DefaultThreadFormatter
-//            .stackTraceFormatter(MyStackTraceFormatter()) // 指定调用栈信息格式化器，默认为 DefaultStackTraceFormatter
-            .borderFormatter(MyBorderFormatter()) // 指定边框格式化器，默认为 DefaultBorderFormatter
-//            .addObjectFormatter(
-//                AnyClass::class.java,  // 为指定类型添加对象格式化器
-//                AnyClassObjectFormatter()
-//            ) // 默认使用 Object.toString()
-//            .addInterceptor(
-//                BlacklistTagsFilterInterceptor( // 添加黑名单 TAG 过滤器
-//                    "blacklist1", "blacklist2", "blacklist3"
-//                )
-//            )
-//            .addInterceptor(MyInterceptor()) // 添加一个日志拦截器
-            .build()
-
+        val config = if (BuildConfig.DEBUG) {
+            LogConfiguration.Builder()
+                .logLevel(LogLevel.ALL) // 指定日志级别，低于该级别的日志将不会被打印，默认为 LogLevel.ALL
+                .tag(LOG_TAG) // 指定 TAG，默认为 "X-LOG"
+                .enableThreadInfo() // 允许打印线程信息，默认禁止
+                .enableStackTrace(2) // 允许打印深度为 2 的调用栈信息，默认禁止
+                .enableBorder() // 允许打印日志边框，默认禁止
+                .borderFormatter(MyBorderFormatter()) // 指定边框格式化器，默认为 DefaultBorderFormatter
+                .build()
+        } else {
+            LogConfiguration.Builder()
+                .logLevel(LogLevel.INFO)
+                .tag(LOG_TAG) // 指定 TAG，默认为 "X-LOG"
+                .enableBorder() // 允许打印日志边框，默认禁止
+                .borderFormatter(MyBorderFormatter()) // 指定边框格式化器，默认为 DefaultBorderFormatter
+                .build()
+        }
 
         val androidPrinter = AndroidPrinter(true) // 通过 android.util.Log 打印日志的打印器
 //        val consolePrinter = ConsolePrinter() // 通过 System.out 打印日志到控制台的打印器
@@ -67,7 +60,8 @@ object Logs {
         XLog.init(                                                 // 初始化 XLog
             config,                                                // 指定日志配置，如果不指定，会默认使用 new LogConfiguration.Builder().build()
             androidPrinter,                                        // 添加任意多的打印器。如果没有添加任何打印器，会默认使用 AndroidPrinter(Android)/ConsolePrinter(java)
-            filePrinter)
+            filePrinter
+        )
     }
 
     @JvmStatic
